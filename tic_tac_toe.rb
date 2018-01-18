87bc6aefe4e378b46a3c7b0f2c54214c76adf8a2
MarkZsombor <markszsombor@gmail.com>
2018-01-18 21:51:42 +0000

    game shows instructions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,11 +6,10 @@ class Board
   def print_board
   end
 
-  def is_winner
-  end
 end
 
 class Player
+  attr_reader :symbol
   def initialize
     @symbol = rand.round == 0 ? "X" : "O"
     puts "You are #{@symbol}"
@@ -18,17 +17,40 @@ class Player
 end
 
 class Game
-  def initialize
-    @player = Player.new
-    @game_board = Board.new
+
+  def self.main
+    game = Game.new
+    game.start_game
   end
 
+  def initialize
+    @game_board = Board.new
+    @current_player = "X"
+  end
 
+  def show_instructions
+    puts "Press the corresponding number to place your marker",
+         "7 | 8 | 9",
+         "- - - - -",
+         "4 | 5 | 6",
+         "- - - - -",
+         "1 | 2 | 3",
+         "First to get a line of 3 wins"
+  end
+
+  def start_game
+    puts "Would you like to play Tic Tac Toe? (y/n)"
+    gets.chomp == "y" ? self.show_instructions : start_game
+    @player = Player.new
+    @comp_symbol = @player.symbol == "X" ? "O" : "X"
+    puts @player.symbol == "X" ? "You go first" : "Computer goes first"
+  end
+
+  def is_winner
+  end
+
+  def is_tie
+  end
 end
 
-def start_game
-  puts "Would you like to play Tic Tac Toe? (y/n)"
-  gets.chomp == "y" ? game = Game.new : start_game
-end
-
-start_game
+Game.main
